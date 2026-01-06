8de0b1f6ae37038348cb3d469d21fb1c270dfb5f
Ildar Zaripov <il.zaripov94@gmail.com>
2026-01-06 15:24:22 +0300

    feat: добавлено логирование в конф-ию

--- src/microservices/proxy/src/main/kotlin/ru/ildar/yandex/gateway/properties/GradualProperties.kt
+++ src/microservices/proxy/src/main/kotlin/ru/ildar/yandex/gateway/properties/GradualProperties.kt
@@ -1,11 +1,27 @@
 package ru.ildar.yandex.gateway.properties
 
+import jakarta.annotation.PostConstruct
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties(prefix = "app.gradual")
 class GradualProperties(enabled: String, movieServicePercent: Int) {
 
+    companion object {
+        val log: Logger = LoggerFactory.getLogger(GradualProperties::class.java)
+    }
+
     val moviePercent = if (enabled == "true") movieServicePercent else 100
     val monolithPercent = 100 - moviePercent
+
+    override fun toString(): String {
+        return "MoviePercent: $moviePercent; MonolithPercent $monolithPercent"
+    }
+
+    @PostConstruct
+    fun postConstruct() {
+        log.error(toString())
+    }
 
 }

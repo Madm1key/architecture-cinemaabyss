e9b49577e297a7f2b40b2bcc9e78c5a7ac19a03a
Ildar <il.zaripov94@gmail.com>
2026-01-07 17:55:50 +0300

    feat: добавлен метод для создания фильма

--- src/microservices/proxy/src/main/kotlin/ru/ildar/yandex/gateway/controller/MovieController.kt
+++ src/microservices/proxy/src/main/kotlin/ru/ildar/yandex/gateway/controller/MovieController.kt
@@ -1,9 +1,8 @@
 package ru.ildar.yandex.gateway.controller
 
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import ru.ildar.yandex.gateway.dto.CreateMovieDto
 import ru.ildar.yandex.gateway.dto.MovieDto
 import ru.ildar.yandex.gateway.service.Movie
 import ru.ildar.yandex.gateway.service.ServiceProvider
@@ -19,5 +18,9 @@ class MovieController(private val serviceProvider: ServiceProvider<Movie>) {
     @GetMapping("/api/movies", params = ["id"])
     fun getMovie(@RequestParam(required = true) id: String): ResponseEntity<MovieDto> =
         ResponseEntity.ok(serviceProvider.getService().getMovie(id))
+
+    @PostMapping("/api/movies")
+    fun createMovie(@RequestBody createMovieDto: CreateMovieDto): ResponseEntity<MovieDto> =
+        ResponseEntity.ok(serviceProvider.getService().createMovie(createMovieDto))
 
 }
